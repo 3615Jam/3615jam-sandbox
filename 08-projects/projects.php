7c8d7b6bf61b2dbd89116abe4b9e7f2d1ba3b37c
Jean-Marc <garciajeanmarc@gmail.com>
2022-06-04 00:42:38 +0200

    updated cards styles

--- 08-projects/projects.php
+++ 08-projects/projects.php
@@ -5,18 +5,24 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>### PROJECTS ###</title>
+    <title>### PROJECTS - PHP ###</title>
     <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="projects-style.css" />
     <!-- <script src="projects.js" defer></script> -->
 </head>
 
 <body>
-    <header></header>
+    <header>
+        <nav class="m-5 d-flex justify-content-center">
+            <div class="m-1 d-flex align-items-center">▶️</div>
+            <div class="m-1 d-flex flex-column">
+                <a href="projects.html" class="m-1 btn btn-outline-primary">PROJECTS - HTML</a>
+                <a href="projects.php" class="m-1 btn btn-info disabled">PROJECTS - PHP</a>
+                <a href="cards.html" class="m-1 btn btn-outline-success">CARDS STYLES</a>
+            </div>
+        </nav>
+    </header>
     <main>
-        <section class="m-5 d-flex flex-wrap justify-content-center">
-            <a href="cards.html" class="btn btn-success">CARDS STYLES</a>
-        </section>
         <!-- LIGHT MODE SECTION -->
         <section id="light_mode" class="m-5 d-flex flex-wrap justify-content-center">
             <?php
@@ -25,20 +31,11 @@
             // on le décode 
             $proj_details = json_decode($projects, true);
             // test pour confirmer les données récupérées 
-            var_dump($proj_details);
+            // var_dump($proj_details);
             // pour chaque projets, on crée une carte et on la rempli avec les valeurs du json
             foreach ($proj_details as $key_proj => $val_proj) {
                 // test pour confirmer les données récupérées 
-                // echo $key . " : " . $val['name'] . "<br />";
-                // foreach ($val_proj['techno'] as $key_tech => $val_tech) {
-                // test pour confirmer les données récupérées 
-                // echo $key_tech . " : " . $val_tech . "<br />";
-                // }
-                // if (in_array("HTML", $val_proj['techno'])) {
-                //     $pill = "<span class=\"badge rounded-pill bg-primary\">HTML</span>";
-                // }
-
-
+                // echo $key_proj . " : " . $val_proj['name'] . "<br />";
                 $proj_card = '
                 <div class="card text-center m-3 bg-light text-dark" style="max-width: 18rem">
                     <div class="card-header"><img src="../img/3615jam-320-bigborder.png" class="card-img-top" /></div>
@@ -46,8 +43,10 @@
                         <h5 class="card-title">' . $val_proj['name'] . '</h5>
                         <p class="card-text">
                 ';
+                // on crée des 'pills' colorées pour chaque techno utilisée dans chaque projet et on les rajoute à la carte
                 foreach ($val_proj['techno'] as $key_tech => $val_tech) {
-                    echo $val_tech;
+                    // test pour confirmer les données récupérées 
+                    // echo $val_tech;
                     switch ($val_tech) {
                         case "HTML":
                             $pill = "primary";
@@ -67,20 +66,16 @@
                         default:
                             echo "Bizarre, il semble qu'aucune techno n'ait été utilisée sur ce projet.";
                     }
-                    $proj_card .= '<span class="badge rounded-pill bg-' . $pill . '">' . $val_tech . '</span>';
+                    $proj_card .= '<span class="m-1 badge rounded-pill bg-' . $pill . '">' . $val_tech . '</span>';
                 }
+                // on finalise la carte avec la description du projet 
                 $proj_card .= '
                 </p>
                 <p class="card-text description">' . $val_proj['description'] . '</p>
                 </div>
-                </div>';
-
-
-
-
-
-
-
+                </div>
+                ';
+                // on affiche la carte complète 
                 echo $proj_card;
             }
             ?>
