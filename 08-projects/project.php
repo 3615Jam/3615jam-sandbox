da597e0845d4d5a6ca2adedbf11ffadf2a95afc2
Jean-Marc <garciajeanmarc@gmail.com>
2022-06-15 14:18:12 +0200

    each project linked to project.php with own infos

--- 08-projects/project.php
+++ 08-projects/project.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>### PROJECTS - PHP ###</title>
+    <title>### PROJECT XXX ###</title>
     <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="projects-style.css" />
     <script src="projects.js" defer></script>
@@ -16,9 +16,9 @@
         <nav class="m-5 d-flex justify-content-center">
             <div class="m-1 d-flex align-items-center">▶️</div>
             <div class="m-1 d-flex flex-column">
-                <a href="projects.html" class="m-1 btn btn-outline-primary">PROJECTS - HTML</a>
-                <a href="projects.php" class="m-1 btn btn-info disabled">PROJECTS - PHP</a>
-                <a href="cards.html" class="m-1 btn btn-outline-success">CARDS STYLES</a>
+                <!-- <a href="projects.html" class="m-1 btn btn-outline-primary">PROJECTS - HTML</a> -->
+                <a href="projects.php" class="m-1 btn btn-info">Back to PROJECTS - PHP</a>
+                <!-- <a href="cards.html" class="m-1 btn btn-outline-success">CARDS STYLES</a> -->
             </div>
         </nav>
     </header>
@@ -32,24 +32,32 @@
             $projects = file_get_contents("projects.json");
             $proj_details = json_decode($projects, true);
             // test pour confirmer les données récupérées 
-            // var_dump($proj_details);
+            // var_dump($proj_details[0]['logo']);
             // --------------------------------------------------
-            // on boucle pour créer une carte par projet : 
-            foreach ($proj_details as $key_proj => $val_proj) {
+            // on récupère les infos du projet sélectionné à la page précédente ... 
+            // ... pour ce faire, on utilise sa position dans le JSON ... 
+            // ... qu'on fait transiter dans l'URL via la variable superglobale $_GET['proj_id'] ...
+            // ... Si cette variable existe et qu'elle est inférieure à la longueur du JSON des projets ... 
+            if (isset($_GET['proj_id']) && ($_GET['proj_id']) < count($proj_details)) {
+                // ... alors on définit '$val_proj' qui contient les infos du projet sélectionné 
+                $val_proj = $proj_details[($_GET['proj_id'])];
                 // --------------------------------------------------
-                // début de la carte 
+                // puis on crée une nouvelle carte avec les éléments de '$val_proj'
                 $proj_card = '
-                    <div class="card text-center m-3 bg-light text-dark" style="width: 18rem; height: 26rem">
-                        <div class="card-header"><img src="' . $val_proj['logo'] . '" class="card-img-top" alt="project logo" /></div>
-                        <div class="card-body">
-                            <h5 class="card-title m-3"><a href="project.php?proj_id=' . $key_proj . '" class="card-link">' . $val_proj['name'] . '</a></h5>
-                            <p class="card-text description">' . $val_proj['summary'] . '</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="d-flex justify-content-center">
+                <div class="card text-center m-3 bg-light text-dark" style="width: 50%">
+                    <div class="card-header"><img src="' . $val_proj['logo'] . '" class="card-img-top" alt="project logo" /></div>
+                    <div class="card-body">
+                        <h5 class="card-title m-3">' . $val_proj['name'] . '</a></h5>
+                        <p class="card-text description">' . $val_proj['summary'] . '</p>
+                        <p class="card-text description">' . $val_proj['description'] . '</p>
+                        <p><a href="/projects/dwwm-samproject" class="m-1 btn btn-outline-info">Test lien externe</a></p>
+                        <a href="' . $val_proj['link'] . '" class="m-1 btn btn-outline-primary">Voir le projet</a>
+                    </div>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-center">
                 ';
                 // --------------------------------------------------
-                // on boucle à nouveau pour créer des 'pills' colorées pour chaque techno utilisée dans chaque projet 
+                // on boucle pour créer des 'pills' colorées pour chaque techno utilisée dans chaque projet 
                 foreach ($val_proj['techno'] as $key_tech => $val_tech) {
                     // test pour confirmer les données récupérées 
                     // echo $val_tech;
@@ -78,13 +86,18 @@
                 // --------------------------------------------------
                 // on finalise la carte 
                 $proj_card .= '
-                            </div>
                         </div>
                     </div>
+                </div>
                 ';
                 // on affiche la carte complétée 
                 echo $proj_card;
+                // --------------------------------------------------
+            } else {
+                // sinon on affiche un message d'erreur 
+                echo ("Oups !!! Quelque chose ne s'est pas bien passé !!!");
             }
+
             ?>
 
         </section>
